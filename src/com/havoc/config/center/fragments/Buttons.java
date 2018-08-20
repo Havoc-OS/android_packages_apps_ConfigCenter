976c63d3d7651e690d83c7327e533d1de405b22f
xyyx <xyyx@mail.ru>
2018-08-20 16:33:58 +0800

    ConfigCenter: Use switch for devices without variable button brightness

--- src/com/havoc/config/center/fragments/Buttons.java
+++ src/com/havoc/config/center/fragments/Buttons.java
@@ -24,25 +24,69 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import androidx.preference.*;
 
+import com.android.internal.logging.nano.MetricsProto;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.havoc.support.preferences.SystemSettingSeekBarPreference;
+import com.havoc.support.preferences.SystemSettingSwitchPreference;
 
 public class Buttons extends SettingsPreferenceFragment implements
     Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
+    private static final String KEY_BUTTON_BRIGHTNESS_SW = "button_brightness_sw";
+
+    private SystemSettingSeekBarPreference mButtonBrightness;
+    private SystemSettingSwitchPreference mButtonBrightness_sw;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.config_center_buttons);
-        PreferenceScreen prefSet = getPreferenceScreen();
-        final Resources res = getResources();
+
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final boolean variableBrightness = getResources().getBoolean(
+                com.android.internal.R.bool.config_deviceHasVariableButtonBrightness);
+
+        mButtonBrightness =
+                (SystemSettingSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+        mButtonBrightness_sw =
+                (SystemSettingSwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS_SW);
+
+        if (variableBrightness) {
+            prefScreen.removePreference(mButtonBrightness_sw);
+            if (mButtonBrightness != null) {
+                int ButtonBrightness = Settings.System.getInt(getContentResolver(),
+                        Settings.System.BUTTON_BRIGHTNESS, 255);
+                mButtonBrightness.setValue(ButtonBrightness / 1);
+                mButtonBrightness.setOnPreferenceChangeListener(this);
+            }
+        } else {
+            prefScreen.removePreference(mButtonBrightness);
+            if (mButtonBrightness_sw != null) {
+                mButtonBrightness_sw.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.BUTTON_BRIGHTNESS, 1) == 1));
+                mButtonBrightness_sw.setOnPreferenceChangeListener(this);
+            }
+        }
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mButtonBrightness) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BUTTON_BRIGHTNESS, value * 1);
+            return true;
+        } else if (preference == mButtonBrightness_sw) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
