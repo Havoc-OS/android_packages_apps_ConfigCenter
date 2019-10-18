6c4fbdb687dcd3acc98e924889f263fa055c8349
Mazda-- <alex@dirtyunicorns.com>
2019-10-19 00:19:41 +0530

    ConfigCenter: Make powermenu lockscreen visibility configurable [2/2]
    Change-Id: I1ed173ef0a621b6a03dbe076ad48d2e34157b6b2

--- src/com/havoc/config/center/fragments/PowerMenu.java
+++ src/com/havoc/config/center/fragments/PowerMenu.java
@@ -15,12 +15,16 @@
  */
 package com.havoc.config.center.fragments;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.UserInfo;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
@@ -33,13 +37,28 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.havoc.config.center.preferences.Utils;
 
-public class PowerMenu extends SettingsPreferenceFragment {
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerMenu extends SettingsPreferenceFragment     
+                implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "PowerMenu";
 
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String KEY_POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
+    private static final String KEY_POWERMENU_LS_REBOOT = "powermenu_ls_reboot";
+    private static final String KEY_POWERMENU_LS_ADVANCED_REBOOT = "powermenu_ls_advanced_reboot";
+    private static final String KEY_POWERMENU_LS_SCREENSHOT = "powermenu_ls_screenshot";
+    private static final String KEY_POWERMENU_LS_TORCH = "powermenu_ls_torch";
 
     private SwitchPreference mPowermenuTorch;
+    private SwitchPreference mPowerMenuLockscreen;
+    private SwitchPreference mPowerMenuReboot;
+    private SwitchPreference mPowerMenuAdvancedReboot;
+    private SwitchPreference mPowerMenuScreenshot;
+    private SwitchPreference mPowerMenuLSTorch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,11 +73,38 @@ public class PowerMenu extends SettingsPreferenceFragment {
         mPowermenuTorch.setOnPreferenceChangeListener(this);
         if (!Utils.deviceSupportsFlashLight(getActivity())) {
             prefScreen.removePreference(mPowermenuTorch);
+            prefScreen.removePreference(mPowerMenuLSTorch);
         } else {
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
 
+        mPowerMenuLockscreen = (SwitchPreference) findPreference(KEY_POWERMENU_LOCKSCREEN);
+        mPowerMenuLockscreen.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1));
+        mPowerMenuLockscreen.setOnPreferenceChangeListener(this);
+
+        mPowerMenuReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_REBOOT);
+        mPowerMenuReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_REBOOT, 1) == 1));
+        mPowerMenuReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuAdvancedReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_ADVANCED_REBOOT);
+        mPowerMenuAdvancedReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 0) == 1));
+        mPowerMenuAdvancedReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuScreenshot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_SCREENSHOT);
+        mPowerMenuScreenshot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_SCREENSHOT, 0) == 1));
+        mPowerMenuScreenshot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuLSTorch = (SwitchPreference) findPreference(KEY_POWERMENU_LS_TORCH);
+        mPowerMenuLSTorch.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_TORCH, 0) == 1));
+        mPowerMenuLSTorch.setOnPreferenceChangeListener(this);
+
+        updateLockscreen();
     }
 	
     @Override
@@ -68,6 +114,32 @@ public class PowerMenu extends SettingsPreferenceFragment {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
             return true;
+        } else if (preference == mPowerMenuLockscreen) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LOCKSCREEN, value ? 1 : 0);
+            updateLockscreen();
+            return true;
+        } else if (preference == mPowerMenuReboot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuAdvancedReboot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_ADVANCED_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuScreenshot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_SCREENSHOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuLSTorch) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_TORCH, value ? 1 : 0);
+            return true;
         }
         return false;
     }
@@ -75,5 +147,22 @@ public class PowerMenu extends SettingsPreferenceFragment {
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    private void updateLockscreen() {
+        boolean lockscreenOptions = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1;
+
+        if (lockscreenOptions) {
+            mPowerMenuReboot.setEnabled(true);
+            mPowerMenuAdvancedReboot.setEnabled(true);
+            mPowerMenuScreenshot.setEnabled(true);
+            mPowerMenuLSTorch.setEnabled(true);
+        } else {
+            mPowerMenuReboot.setEnabled(false);
+            mPowerMenuAdvancedReboot.setEnabled(false);
+            mPowerMenuScreenshot.setEnabled(false);
+            mPowerMenuLSTorch.setEnabled(false);
+        }
     }
 }
