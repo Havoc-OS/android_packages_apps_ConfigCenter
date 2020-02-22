9370de5cc0e95c374d173834152ba10bc32c27e5
Alex <du.alexcruz@gmail.com>
2020-02-22 03:42:01 -0500

    ConfigCenter: Navbar: Hide custom application preference if not in use
    No reason to show these preferences not in use.
    
    Change-Id: I71133e4e96b177cd979fde0b2199f6ca9f62315a

--- src/com/havoc/config/center/fragments/Buttons.java
+++ src/com/havoc/config/center/fragments/Buttons.java
@@ -25,7 +25,13 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.widget.Toast;
-import androidx.preference.*;
+
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto; 
 import com.android.internal.util.havoc.Utils;
@@ -337,17 +343,17 @@ public class Buttons extends SettingsPreferenceFragment
         navbarCheck();
         customAppCheck();
 
-        mBackLongPressCustomApp.setEnabled(mBackLongPress.getEntryValues()
+        mBackLongPressCustomApp.setVisible(mBackLongPress.getEntryValues()
                 [backlongpress].equals("16"));
-        mBackDoubleTapCustomApp.setEnabled(mBackDoubleTap.getEntryValues()
+        mBackDoubleTapCustomApp.setVisible(mBackDoubleTap.getEntryValues()
                 [backdoubletap].equals("16"));
-        mHomeLongPressCustomApp.setEnabled(mHomeLongPress.getEntryValues()
+        mHomeLongPressCustomApp.setVisible(mHomeLongPress.getEntryValues()
                 [homelongpress].equals("16"));
-        mHomeDoubleTapCustomApp.setEnabled(mHomeDoubleTap.getEntryValues()
+        mHomeDoubleTapCustomApp.setVisible(mHomeDoubleTap.getEntryValues()
                 [homedoubletap].equals("16"));
-        mAppSwitchLongPressCustomApp.setEnabled(mAppSwitchLongPress.getEntryValues()
+        mAppSwitchLongPressCustomApp.setVisible(mAppSwitchLongPress.getEntryValues()
                 [appswitchlongpress].equals("16"));
-        mAppSwitchDoubleTapCustomApp.setEnabled(mAppSwitchDoubleTap.getEntryValues()
+        mAppSwitchDoubleTapCustomApp.setVisible(mAppSwitchDoubleTap.getEntryValues()
                 [appswitchdoubletap].equals("16"));
     }
 
@@ -401,7 +407,7 @@ public class Buttons extends SettingsPreferenceFragment
             mBackLongPress.setSummary(
                     mBackLongPress.getEntries()[index]);
             customAppCheck();
-            mBackLongPressCustomApp.setEnabled(mBackLongPress.getEntryValues()
+            mBackLongPressCustomApp.setVisible(mBackLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mBackDoubleTap) {
@@ -412,7 +418,7 @@ public class Buttons extends SettingsPreferenceFragment
             int index = mBackDoubleTap.findIndexOfValue((String) objValue);
             mBackDoubleTap.setSummary(
                     mBackDoubleTap.getEntries()[index]);
-            mBackDoubleTapCustomApp.setEnabled(mBackDoubleTap.getEntryValues()
+            mBackDoubleTapCustomApp.setVisible(mBackDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mHomeLongPress) {
@@ -423,7 +429,7 @@ public class Buttons extends SettingsPreferenceFragment
             int index = mHomeLongPress.findIndexOfValue((String) objValue);
             mHomeLongPress.setSummary(
                     mHomeLongPress.getEntries()[index]);
-            mHomeLongPressCustomApp.setEnabled(mHomeLongPress.getEntryValues()
+            mHomeLongPressCustomApp.setVisible(mHomeLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mHomeDoubleTap) {
@@ -434,7 +440,7 @@ public class Buttons extends SettingsPreferenceFragment
             int index = mHomeDoubleTap.findIndexOfValue((String) objValue);
             mHomeDoubleTap.setSummary(
                     mHomeDoubleTap.getEntries()[index]);
-            mHomeDoubleTapCustomApp.setEnabled(mHomeDoubleTap.getEntryValues()
+            mHomeDoubleTapCustomApp.setVisible(mHomeDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mAppSwitchLongPress) {
@@ -445,7 +451,7 @@ public class Buttons extends SettingsPreferenceFragment
             int index = mAppSwitchLongPress.findIndexOfValue((String) objValue);
             mAppSwitchLongPress.setSummary(
                     mAppSwitchLongPress.getEntries()[index]);
-            mAppSwitchLongPressCustomApp.setEnabled(mAppSwitchLongPress.getEntryValues()
+            mAppSwitchLongPressCustomApp.setVisible(mAppSwitchLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mAppSwitchDoubleTap) {
@@ -456,7 +462,7 @@ public class Buttons extends SettingsPreferenceFragment
             int index = mAppSwitchDoubleTap.findIndexOfValue((String) objValue);
             mAppSwitchDoubleTap.setSummary(
                     mAppSwitchDoubleTap.getEntries()[index]);
-            mAppSwitchDoubleTapCustomApp.setEnabled(mAppSwitchDoubleTap.getEntryValues()
+            mAppSwitchDoubleTapCustomApp.setVisible(mAppSwitchDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mMenuLongPress) {
