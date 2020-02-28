3655266e10854769913bc0f406270703f985ebec
Anushek Prasal <anushekprasal@gmail.com>
2020-02-29 03:09:08 +0530

    ConfigCenter: Cleanup HW keys customizations

--- src/com/havoc/config/center/fragments/Buttons.java
+++ src/com/havoc/config/center/fragments/Buttons.java
@@ -15,123 +15,40 @@
  */
 package com.havoc.config.center.fragments;
 
+import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.Vibrator;
+import android.os.SystemProperties;
 import android.provider.Settings;
-import androidx.preference.*;
 import android.widget.Toast;
+import androidx.preference.*;
 
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.util.hwkeys.ActionConstants;
-import com.android.internal.util.hwkeys.ActionUtils;
+import com.android.internal.logging.nano.MetricsProto; 
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-import com.havoc.config.center.preferences.ActionFragment;
-import com.havoc.support.preferences.SystemSettingSeekBarPreference;
-import com.havoc.support.preferences.SystemSettingSwitchPreference;
+public class Buttons extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
-public class Buttons extends ActionFragment implements Preference.OnPreferenceChangeListener {
+    public static final String TAG = "Buttons";
 
-    //Keys
-    private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
-    private static final String KEY_BUTTON_BRIGHTNESS_SW = "button_brightness_sw";
-    private static final String KEY_BACKLIGHT_TIMEOUT = "backlight_timeout";
-    private static final String HWKEY_DISABLE = "hardware_keys_disable";
     private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+    private static final String NAV_BAR_LAYOUT = "nav_bar_layout";
+    private static final String SYSUI_NAV_BAR = "sysui_nav_bar";
 
-    // category keys
-    private static final String CATEGORY_HWKEY = "hardware_keys";
-    private static final String CATEGORY_HOME = "home_key";
-    private static final String CATEGORY_MENU = "menu_key";
-    private static final String CATEGORY_BACK = "back_key";
-    private static final String CATEGORY_ASSIST = "assist_key";
-    private static final String CATEGORY_APPSWITCH = "app_switch_key";
-
-    // Masks for checking presence of hardware keys.
-    // Must match values in frameworks/base/core/res/res/values/config.xml
-    // Masks for checking presence of hardware keys.
-    // Must match values in frameworks/base/core/res/res/values/config.xml
-    public static final int KEY_MASK_HOME = 0x01;
-    public static final int KEY_MASK_BACK = 0x02;
-    public static final int KEY_MASK_MENU = 0x04;
-    public static final int KEY_MASK_ASSIST = 0x08;
-    public static final int KEY_MASK_APP_SWITCH = 0x10;
-    public static final int KEY_MASK_CAMERA = 0x20;
-    public static final int KEY_MASK_VOLUME = 0x40;
-
-    private ListPreference mBacklightTimeout;
-    private SystemSettingSeekBarPreference mButtonBrightness;
-    private SystemSettingSwitchPreference mButtonBrightness_sw;
-    private SystemSettingSwitchPreference mHwKeyDisable;
+    private ContentResolver resolver;
     private ListPreference mTorchPowerButton;
+    private ListPreference mNavBarLayout;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.config_center_buttons);
+        resolver = getActivity().getContentResolver();
 
-        final Resources res = getResources();
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-
-        final boolean needsNavbar = ActionUtils.hasNavbarByDefault(getActivity());
-        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
-                .findPreference(CATEGORY_HWKEY);
-        int keysDisabled = 0;
-        if (!needsNavbar) {
-            mHwKeyDisable = (SystemSettingSwitchPreference) findPreference(HWKEY_DISABLE);
-            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
-                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
-                    UserHandle.USER_CURRENT);
-            mHwKeyDisable.setChecked(keysDisabled != 0);
-            mHwKeyDisable.setOnPreferenceChangeListener(this);
-
-            final boolean variableBrightness = getResources().getBoolean(
-                    com.android.internal.R.bool.config_deviceHasVariableButtonBrightness);
-
-            mBacklightTimeout =
-                    (ListPreference) findPreference(KEY_BACKLIGHT_TIMEOUT);
-
-            mButtonBrightness =
-                    (SystemSettingSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
-
-            mButtonBrightness_sw =
-                    (SystemSettingSwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS_SW);
-
-                if (mBacklightTimeout != null) {
-                    mBacklightTimeout.setOnPreferenceChangeListener(this);
-                    int BacklightTimeout = Settings.System.getInt(getContentResolver(),
-                            Settings.System.BUTTON_BACKLIGHT_TIMEOUT, 5000);
-                    mBacklightTimeout.setValue(Integer.toString(BacklightTimeout));
-                    mBacklightTimeout.setSummary(mBacklightTimeout.getEntry());
-                }
-
-                if (variableBrightness) {
-                    hwkeyCat.removePreference(mButtonBrightness_sw);
-                    if (mButtonBrightness != null) {
-                        int ButtonBrightness = Settings.System.getInt(getContentResolver(),
-                                Settings.System.BUTTON_BRIGHTNESS, 255);
-                        mButtonBrightness.setValue(ButtonBrightness / 1);
-                        mButtonBrightness.setOnPreferenceChangeListener(this);
-                    }
-                } else {
-                    hwkeyCat.removePreference(mButtonBrightness);
-                    if (mButtonBrightness_sw != null) {
-                        mButtonBrightness_sw.setChecked((Settings.System.getInt(getContentResolver(),
-                                Settings.System.BUTTON_BRIGHTNESS, 1) == 1));
-                        mButtonBrightness_sw.setOnPreferenceChangeListener(this);
-                    }
-                }
-        } else {
-            prefScreen.removePreference(hwkeyCat);
-        }
-
-        // screen off torch
         mTorchPowerButton = (ListPreference) findPreference(TORCH_POWER_BUTTON_GESTURE);
         int mTorchPowerButtonValue = Settings.Secure.getInt(resolver,
                 Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0);
@@ -139,95 +56,25 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
         mTorchPowerButton.setSummary(mTorchPowerButton.getEntry());
         mTorchPowerButton.setOnPreferenceChangeListener(this);
 
-        // bits for hardware keys present on device
-        final int deviceKeys = getResources().getInteger(
-                com.android.internal.R.integer.config_deviceHardwareKeys);
-
-        // read bits for present hardware keys
-        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
-        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
-        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
-        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
-        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
-
-        // load categories and init/remove preferences based on device
-        // configuration
-        final PreferenceCategory backCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
-        final PreferenceCategory homeCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
-        final PreferenceCategory menuCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
-        final PreferenceCategory assistCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
-        final PreferenceCategory appSwitchCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
-
-        // back key
-        if (!hasBackKey) {
-            prefScreen.removePreference(backCategory);
+        mNavBarLayout = (ListPreference) findPreference(NAV_BAR_LAYOUT);
+        mNavBarLayout.setOnPreferenceChangeListener(this);
+        String navBarLayoutValue = Settings.Secure.getString(resolver, SYSUI_NAV_BAR);
+        if (navBarLayoutValue != null) {
+            mNavBarLayout.setValue(navBarLayoutValue);
+        } else {
+            mNavBarLayout.setValueIndex(0);
         }
 
-        // home key
-        if (!hasHomeKey) {
-            prefScreen.removePreference(homeCategory);
-        }
-
-        // App switch key (recents)
-        if (!hasAppSwitchKey) {
-            prefScreen.removePreference(appSwitchCategory);
-        }
-
-        // menu key
-        if (!hasMenuKey) {
-            prefScreen.removePreference(menuCategory);
-        }
-
-        // search/assist key
-        if (!hasAssistKey) {
-            prefScreen.removePreference(assistCategory);
-        }
-
-        // let super know we can load ActionPreferences
-        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
-
-        // load preferences first
-        setActionPreferencesEnabled(keysDisabled == 0);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        resolver = getActivity().getContentResolver();
         boolean DoubleTapPowerGesture = Settings.Secure.getInt(resolver,
                     Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1) == 0;
-        if (preference == mBacklightTimeout) {
-            String BacklightTimeout = (String) newValue;
-            int BacklightTimeoutValue = Integer.parseInt(BacklightTimeout);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BUTTON_BACKLIGHT_TIMEOUT, BacklightTimeoutValue);
-            int BacklightTimeoutIndex = mBacklightTimeout
-                    .findIndexOfValue(BacklightTimeout);
-            mBacklightTimeout
-                    .setSummary(mBacklightTimeout.getEntries()[BacklightTimeoutIndex]);
-            return true;
-        } else if (preference == mButtonBrightness) {
-            int value = (Integer) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BUTTON_BRIGHTNESS, value * 1);
-            return true;
-        } else if (preference == mButtonBrightness_sw) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
-            return true;
-        } else if (preference == mHwKeyDisable) {
-            boolean value = (Boolean) newValue;
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
-                    value ? 1 : 0);
-            setActionPreferencesEnabled(!value);
-            return true;
-        } else if (preference == mTorchPowerButton) {
-            int mTorchPowerButtonValue = Integer.valueOf((String) newValue);
-            int index = mTorchPowerButton.findIndexOfValue((String) newValue);
+        if (preference == mTorchPowerButton) {
+            int mTorchPowerButtonValue = Integer.valueOf((String) objValue);
+            int index = mTorchPowerButton.findIndexOfValue((String) objValue);
             mTorchPowerButton.setSummary(
                     mTorchPowerButton.getEntries()[index]);
             Settings.Secure.putInt(resolver, Settings.Secure.TORCH_POWER_BUTTON_GESTURE,
@@ -241,13 +88,11 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
                     Toast.LENGTH_SHORT).show();
             }
             return true;
+        } else if (preference == mNavBarLayout) {
+            Settings.Secure.putString(resolver, SYSUI_NAV_BAR, (String) objValue);
+            return true;
         }
         return false;
-    }
-
-    @Override
-    protected boolean usesExtendedActionsList() {
-        return true;
     }
 
     @Override
