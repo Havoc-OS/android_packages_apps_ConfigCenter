a28838a33ddf4bdccf220b8f7681da2d15105e78
Anushek Prasal <anushekprasal@gmail.com>
2020-09-13 00:03:19 +0530

    Revert "ConfigCenter: make aspect ratio hack configable on app level [2/2]"
    This reverts commit 521356814bd9843de2f429083ad28edf5ceabb58.

--- src/com/havoc/config/center/fragments/Screen.java
+++ src/com/havoc/config/center/fragments/Screen.java
@@ -24,7 +24,6 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.text.TextUtils;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -35,8 +34,6 @@ import com.android.internal.util.havoc.Utils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.havoc.config.center.preferences.AppMultiSelectListPreference;
-import com.havoc.config.center.preferences.ScrollAppsViewPreference;
 import com.havoc.support.preferences.CustomSeekBarPreference;
 import com.havoc.support.preferences.SecureSettingSwitchPreference;
 
@@ -49,17 +46,11 @@ import java.util.List;
 public class Screen extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
-    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
-    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
-    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
     private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
     private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
     private static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
     private static final String KEY_CUTOUT_CATEGORY = "cutout_category";
 
-    private AppMultiSelectListPreference mAspectRatioAppsSelect;
-    private ScrollAppsViewPreference mAspectRatioApps;
     private CustomSeekBarPreference mCornerRadius;
     private CustomSeekBarPreference mContentPadding;
     private SecureSettingSwitchPreference mRoundedFwvals;
@@ -84,32 +75,6 @@ public class Screen extends SettingsPreferenceFragment implements
             (PreferenceCategory) getPreferenceScreen().findPreference(KEY_CUTOUT_CATEGORY);
         if (!Utils.hasNotch(getContext())) {
             getPreferenceScreen().removePreference(cutoutCategory);
-        }
-
-        final PreferenceCategory aspectRatioCategory =
-            (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
-        final boolean supportMaxAspectRatio =
-            getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
-        if (!supportMaxAspectRatio) {
-            getPreferenceScreen().removePreference(aspectRatioCategory);
-        } else {
-            mAspectRatioAppsSelect =
-                (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
-            mAspectRatioApps =
-                (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
-            final String valuesString = Settings.System.getString(getContentResolver(),
-                Settings.System.ASPECT_RATIO_APPS_LIST);
-            List<String> valuesList = new ArrayList<String>();
-            if (!TextUtils.isEmpty(valuesString)) {
-                valuesList.addAll(Arrays.asList(valuesString.split(":")));
-                mAspectRatioApps.setVisible(true);
-                mAspectRatioApps.setValues(valuesList);
-                mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.aspect_ratio_footer_text);
-            } else {
-                mAspectRatioApps.setVisible(false);
-            }
-            mAspectRatioAppsSelect.setValues(valuesList);
-            mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
         }
 
         // Rounded Corner Radius
@@ -137,21 +102,7 @@ public class Screen extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mAspectRatioAppsSelect) {
-            Collection<String> valueList = (Collection<String>) newValue;
-            mAspectRatioApps.setVisible(false);
-            if (valueList != null) {
-                Settings.System.putString(getContentResolver(),
-                    Settings.System.ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
-                mAspectRatioApps.setVisible(true);
-                mAspectRatioApps.setValues(valueList);
-                mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.aspect_ratio_footer_text);
-            } else {
-                Settings.System.putString(getContentResolver(),
-                    Settings.System.ASPECT_RATIO_APPS_LIST, "");
-            }
-            return true;
-        } else if (preference == mCornerRadius) {
+        if (preference == mCornerRadius) {
             Settings.Secure.putIntForUser(getContext().getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
                     (int) newValue, UserHandle.USER_CURRENT);
             return true;
